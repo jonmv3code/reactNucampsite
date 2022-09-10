622afc96755480bb547bf9f17634bf2fc7759d70
Jonmikal Vidal <jonmv3code@gmail.com>
2022-09-10 11:58:12 -0500

    Week 3 workshop

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { commentsReducer } from "../features/comments/commentsSlice";
 import { partnerReducers } from "../features/partners/partnersSlice";
 import { promotionReducers } from "../features/promotions/promotionsSlice";
+import { userReducer } from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
@@ -11,6 +12,7 @@ export const store = configureStore({
     comments: commentsReducer,
     partners: partnerReducers,
     promotions: promotionReducers,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
