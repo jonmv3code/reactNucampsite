ae920074c6dc2b416470cfd5d264202c50caa007
Jonmikal Vidal <jonmv3code@gmail.com>
2022-09-17 15:31:01 -0600

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import { Routes, Route} from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from '../src/pages/CampsitesDirectoryPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "../src/pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='contact' element={<ContactPage />}/>
-        <Route path='directory' element={<CampsitesDirectoryPage />} />
-        <Route
-          path='directory/:campsiteId'
-          element={<CampsiteDetailPage />}
-        />
-        <Route path='about' element={<AboutPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
-    
   );
 }
 
