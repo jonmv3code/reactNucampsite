cc5de1a782f97397eaeb9daeb666240fa38a84f5
Jonmikal Vidal <jonmv3code@gmail.com>
2022-09-18 08:36:18 -0600

    Async Parners

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import CampsitesDirectoryPage from "../src/pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import { fetchCampsites } from "./features/campsites/campsitesSlice";
 import AboutPage from "./pages/AboutPage";
+import { fetchPartners } from "./features/partners/partnersSlice";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
   }, [dispatch]);
 
   return (
